9f7185f54b4568e4cffcd22ad8db9910c02ea607
Tio Jobs <icarojobsoficial@gmail.com>
2022-08-24 16:30:00 -0300

    chore: finished algorithm

---
+++ app.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * To work with binary search, the array should be ordered.
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$startTime = microtime(true);
+
+use Tiojobs\BinarySearch\Algorithms\BinarySearch;
+
+$binarySearch = new BinarySearch();
+
+$myArray = ['Icaro', 'Fabio', 'Antonio'];
+sort($myArray);
+
+try {
+    $position = $binarySearch->find('Antonio', $myArray);
+
+    dump("My array is on position '{$position}' and the value in the ordered array is '{$myArray[$position]}'");
+} catch (Exception $exception) {
+    dump($exception->getMessage());
+} finally {
+    $endTime = microtime(true);
+    $executionTime = round(($endTime - $startTime), 2);
+
+    dump("Execution time of scrips was {$executionTime} seconds");
+}
---
+++ src/Algorithms/BinarySearch.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tiojobs\BinarySearch\Algorithms;
+
+use ArrayIterator;
+use InvalidArgumentException;
+
+class BinarySearch
+{
+    public function find(mixed $needle, array|ArrayIterator $array): int
+    {
+        $low = 0;
+        $high = count($array) - 1;
+
+        while ($low <= $high) {
+            $middle = floor(($low + $high) / 2);
+
+            if ($array[$middle] == $needle) {
+                return (int) $middle;
+            }
+
+            if ($array[$middle] > $needle) {
+                $high = $middle - 1;
+            } else {
+                $low = $middle + 1;
+            }
+        }
+
+        throw new InvalidArgumentException("Valor não encontrado. Ordene o seu array com a função sort(array) e tente novamente.");
+    }
+}
